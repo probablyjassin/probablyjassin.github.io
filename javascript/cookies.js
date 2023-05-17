eb1f67a0da08588c6eb3b34a50f8f5bdc5ef387f
probablyjassin <aouanijassin@gmail.com>
2023-05-17 09:37:16 +0200

    preparing style things ok? ok

--- javascript/cookies.js
+++ javascript/cookies.js
@@ -14,18 +14,18 @@ function setCookie(name, value) {
 	document.cookie = name + "=" + value + ";expires=" + cookieDate.toUTCString() + ";path=/;"; 
 }}
 
-  // Helper function to get cookies
-  function getCookie(name) {
+// Helper function to get cookies
+function getCookie(name) {
 	const cookieString = decodeURIComponent(document.cookie);
 	const cookies = cookieString.split(';');
 	for (let i = 0; i < cookies.length; i++) {
-	  let cookie = cookies[i].trim();
-	  if (cookie.indexOf(name + "=") === 0) {
+		let cookie = cookies[i].trim();
+		if (cookie.indexOf(name + "=") === 0) {
 		return cookie.substring(name.length + 1, cookie.length);
-	  }
+		}
 	}
 	return "";
-  }
+}
 
 //cookie popup things happening here
 agreebutton.addEventListener("click", function() {
