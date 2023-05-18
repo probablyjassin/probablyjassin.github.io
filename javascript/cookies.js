a0202d9b684c2e2d2bcbd9b47603bcb7a62bbda6
probablyjassin <aouanijassin@gmail.com>
2023-05-18 13:34:28 +0200

    fix

--- javascript/cookies.js
+++ javascript/cookies.js
@@ -65,7 +65,7 @@ function resetcookies() {
 		const cookie = cookies[i];
 		const eqPos = cookie.indexOf("=");
 		const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT" + "Domain=.jassin.aouani.de;";
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT" + ";Domain=.jassin.aouani.de;";
 	}
 }
 
