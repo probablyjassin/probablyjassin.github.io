d25c50298c32cd68b8826bfe1ed228dbdeea4634
probablyjassin <aouanijassin@gmail.com>
2024-11-21 19:06:17 +0100

    navbar basically done

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,20 +3,44 @@
 		<NavigationMenu>
 			<NavigationMenuList>
 				<NavigationMenuItem>
-					<NuxtLink to="/docs">
-						<NavigationMenuLink :class="navigationMenuTriggerStyle()"> Documentation </NavigationMenuLink>
+					<NuxtLink to="/">
+						<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+							<Icon name="line-md:home" style="color: black" class="w-5 h-5" />
+							Home
+						</NavigationMenuLink>
 					</NuxtLink>
 				</NavigationMenuItem>
+
 				<NavigationMenuItem>
-					<NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-					<NavigationMenuContent>
-						<NavigationMenuLink>Link</NavigationMenuLink>
-					</NavigationMenuContent>
+					<NuxtLink to="/projects">
+						<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+							<Icon name="line-md:document-code" style="color: black" class="w-5 h-5" />
+							Projects
+						</NavigationMenuLink>
+					</NuxtLink>
+				</NavigationMenuItem>
+
+				<NavigationMenuItem>
+					<NuxtLink to="/blog">
+						<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+							<Icon name="line-md:text-box" style="color: black" class="w-5 h-5" />
+							Blog
+						</NavigationMenuLink>
+					</NuxtLink>
+				</NavigationMenuItem>
+
+				<NavigationMenuItem>
+					<NuxtLink to="https://github.com/probablyjassin">
+						<NavigationMenuLink :class="navigationMenuTriggerStyle()">
+							<Icon name="line-md:github-loop" style="color: black" class="w-5 h-5" />
+							Github
+						</NavigationMenuLink>
+					</NuxtLink>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
 
-		<main class="px-[2em]">
+		<main class="px-6">
 			<slot></slot>
 		</main>
 	</div>
