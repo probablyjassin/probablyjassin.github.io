451cbc760f9516e54fd7714411e5f8fbd03668b0
Jässin Aouani <aouanijassin@gmail.com>
2024-11-21 15:19:26 +0100

    add content not found

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,13 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <div class="text-center py-16">
+          <h1 class="text-4xl font-bold mb-4">Document Not Found</h1>
+          <p class="text-lg mb-4">Sorry, the document you are looking for does not exist.</p>
+          <a href="/" class="text-primary underline">Go back to the homepage</a>
+        </div>
+      </template>
+    </ContentDoc>
   </main>
 </template>
