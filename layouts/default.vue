fd042d267a76f73f9ecdd7fda8c4ce03a9b242da
probablyjassin <aouanijassin@gmail.com>
2024-11-21 20:11:51 +0100

    navbar loading state due to hydration error

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,48 @@
 <template>
 	<div>
 		<nav>
-			<NavigationMenu>
-				<NavigationMenuList>
-					<NavigationMenuItem>
-						<NuxtLink to="/">
-							<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
-								<Icon name="line-md:home" style="color: black" class="w-5 h-5" />
-								Home
-							</NavigationMenuLink>
-						</NuxtLink>
-					</NavigationMenuItem>
+			<ClientOnly v-if="isMounted">
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NuxtLink to="/">
+								<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+									<Icon name="line-md:home" style="color: black" class="w-5 h-5" />
+									Home
+								</NavigationMenuLink>
+							</NuxtLink>
+						</NavigationMenuItem>
 
-					<NavigationMenuItem>
-						<NuxtLink to="/projects">
-							<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
-								<Icon name="line-md:document-code" style="color: black" class="w-5 h-5" />
-								Projects
-							</NavigationMenuLink>
-						</NuxtLink>
-					</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NuxtLink to="/projects">
+								<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+									<Icon name="line-md:document-code" style="color: black" class="w-5 h-5" />
+									Projects
+								</NavigationMenuLink>
+							</NuxtLink>
+						</NavigationMenuItem>
 
-					<NavigationMenuItem>
-						<NuxtLink to="/blog">
-							<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
-								<Icon name="line-md:text-box" style="color: black" class="w-5 h-5" />
-								Blog
-							</NavigationMenuLink>
-						</NuxtLink>
-					</NavigationMenuItem>
+						<NavigationMenuItem>
+							<NuxtLink to="/blog">
+								<NavigationMenuLink :class="navigationMenuTriggerStyle()" class="no-underline">
+									<Icon name="line-md:text-box" style="color: black" class="w-5 h-5" />
+									Blog
+								</NavigationMenuLink>
+							</NuxtLink>
+						</NavigationMenuItem>
 
-					<NavigationMenuItem>
-						<NuxtLink to="https://github.com/probablyjassin">
-							<NavigationMenuLink :class="navigationMenuTriggerStyle()">
-								<Icon name="line-md:github-loop" style="color: black" class="w-5 h-5" />
-								Github
-							</NavigationMenuLink>
-						</NuxtLink>
-					</NavigationMenuItem>
-				</NavigationMenuList>
-			</NavigationMenu>
+						<NavigationMenuItem>
+							<NuxtLink to="https://github.com/probablyjassin">
+								<NavigationMenuLink :class="navigationMenuTriggerStyle()">
+									<Icon name="line-md:github-loop" style="color: black" class="w-5 h-5" />
+									Github
+								</NavigationMenuLink>
+							</NuxtLink>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</ClientOnly>
+			<div v-else class="placeholder"></div>
 		</nav>
 		<Breadcrumb>
 			<BreadcrumbList>
@@ -49,11 +52,7 @@
 				<template v-for="(location, index) in route.fullPath.split('/').filter(Boolean)" :key="index">
 					<BreadcrumbSeparator class="mt-1" />
 					<BreadcrumbItem>
-						<BreadcrumbLink
-							:href="`/${route.fullPath
-								.split('/')
-								.slice(1, index + 1)
-								.join('/')}`">
+						<BreadcrumbLink :href="`/${route.fullPath.split('/').slice(1).join('/')}`">
 							{{ location.charAt(0).toUpperCase() + location.slice(1) }}
 						</BreadcrumbLink>
 					</BreadcrumbItem>
@@ -72,6 +71,28 @@
 	import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 	const route = useRoute();
+
+	const isMounted = ref(false);
+	onMounted(() => {
+		isMounted.value = true;
+	});
 </script>
 
-<style scoped></style>
+<style scoped>
+	.placeholder {
+		height: 40px;
+		margin-bottom: 24px;
+		background-color: #f0f0f0;
+		animation: fadeInOut 0.5s infinite;
+	}
+
+	@keyframes fadeInOut {
+		0%,
+		100% {
+			opacity: 0.5;
+		}
+		50% {
+			opacity: 1;
+		}
+	}
+</style>
