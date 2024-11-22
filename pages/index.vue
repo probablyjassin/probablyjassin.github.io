c02ac11f5c15d9db23a2a299889a71923628c330
Jässin Aouani <aouanijassin@gmail.com>
2024-11-22 12:25:33 +0100

    idk if this works

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <main>
+        <ContentDoc
+            path="https://raw.githubusercontent.com/probablyjassin/blog-content/refs/heads/main/content/main.md" />
+    </main>
+</template>
