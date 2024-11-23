eaeaa3ad0024b5cc720cd653d0bbdd1bb65335d9
probablyjassin <aouanijassin@gmail.com>
2024-11-23 12:45:52 +0100

    fix markdown shenanigans

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
 	devtools: { enabled: false },
 	compatibilityDate: "2024-11-21",
 
-	modules: [
-		"@nuxt/content",
-		"@nuxtjs/tailwindcss",
-		"shadcn-nuxt",
-		"@nuxt/icon",
-		"@nuxthq/studio",
-	],
+	modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/icon", "@nuxthq/studio"],
 	css: ["~/assets/css/main.css"],
 
 	postcss: {
@@ -19,27 +13,15 @@ export default defineNuxtConfig({
 		},
 	},
 
-	content: {
-		sources: {
-			github: {
-				prefix: '',
-				driver: 'github',
-				repo: "probablyjassin/blog-content",
-				branch: "main",
-				dir: "content",
-			},
-		}
-	},
-
 	shadcn: {
 		/**
-			* Prefix for all the imported component
-			*/
+		 * Prefix for all the imported component
+		 */
 		prefix: "",
 		/**
-			* Directory that the component lives in.
-			* @default "./components/ui"
-			*/
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
 		componentDir: "./components/ui",
 	},
 });
