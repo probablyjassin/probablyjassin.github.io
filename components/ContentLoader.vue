0118b389f27b9f4bf36277ad72dd7778dfc4f3c0
Jässin Aouani <aouanijassin@gmail.com>
2024-11-25 09:59:19 +0100

    offload loading to other components and use Suspense

---
+++ components/ContentLoader.vue
@@ -0,0 +1,11 @@
+<template>
+    <ContentDoc>
+        <template #not-found>
+            <div class="text-center py-16">
+                <h1 class="text-4xl font-bold mb-4">Document Not Found</h1>
+                <p class="text-lg mb-4">Sorry, the document you are looking for does not exist.</p>
+                <NuxtLink to="/" class="text-primary underline">Go back to the homepage</NuxtLink>
+            </div>
+        </template>
+    </ContentDoc>
+</template>
