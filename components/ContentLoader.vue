304a980f21f83bd63ce24b514b1f40a409b1fc3c
s5002669 <s5002669@ba-sachsen.de>
2024-11-25 12:47:30 +0100

    improve 404 pages

--- components/ContentLoader.vue
+++ components/ContentLoader.vue
@@ -2,9 +2,9 @@
     <ContentDoc>
         <template #not-found>
             <div class="text-center py-16">
-                <h1 class="text-4xl font-bold mb-4">Document Not Found</h1>
-                <p class="text-lg mb-4">Sorry, the document you are looking for does not exist.</p>
-                <NuxtLink to="/" class="text-primary underline">Go back to the homepage</NuxtLink>
+                <h1 class="text-4xl font-bold mb-4">Blog Post Not Found (404)</h1>
+                <p class="text-lg mb-4">Sorry, the post you are looking for does not exist.</p>
+                <NuxtLink to="/blog" class="text-primary underline">Check out all existing blog posts</NuxtLink>
             </div>
         </template>
     </ContentDoc>
