37095398c34c03e158fd37420c2355f804d9e60f
s5002669 <s5002669@ba-sachsen.de>
2024-11-25 12:47:30 +0100

    improve 404 pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,8 +5,8 @@
 				<ContentDoc>
 					<template #not-found>
 						<div class="text-center py-16">
-							<h1 class="text-4xl font-bold mb-4">Document Not Found</h1>
-							<p class="text-lg mb-4">Sorry, the document you are looking for does not exist.</p>
+							<h1 class="text-4xl font-bold mb-4">Page Not Found (404)</h1>
+							<p class="text-lg mb-4">Sorry, the page you are looking for does not exist.</p>
 							<NuxtLink to="/" class="text-primary underline">Go back to the homepage</NuxtLink>
 						</div>
 					</template>
