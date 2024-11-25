bb64b4f867e3420080e01da6f72d83c82ff65616
Jässin Aouani <aouanijassin@gmail.com>
2024-11-25 10:10:40 +0100

    improved loading performance of blog a lot more

--- components/RelatedPosts.vue
+++ components/RelatedPosts.vue
@@ -1,13 +1,24 @@
 <template>
-    <section v-if="postsState?.length" class="mt-8">
-        <h2 class="text-2xl font-bold mb-4">Did you also read these? 👀</h2>
-        <ul>
-            <li v-for="post in shuffled(postsState.filter((p) => p._path !== $route.path).slice(0, 3))"
-                :key="post._path">
-                <NuxtLink :to="post._path" class="text-primary underline">{{ post.title }}</NuxtLink>
-            </li>
-        </ul>
-    </section>
+    <Suspense>
+        <template #default>
+            <section v-if="posts?.length" class="mt-8">
+                <h2 class="text-2xl font-bold mb-4">Did you also read these? 👀</h2>
+                <ul>
+                    <li v-for="post in shuffled(posts.filter((p) => p._path !== $route.path).slice(0, 3))"
+                        :key="post._path">
+                        <NuxtLink :to="post._path" class="text-primary underline">{{ post.title }}</NuxtLink>
+                    </li>
+                </ul>
+            </section>
+        </template>
+        <template #fallback>
+            <div class="animate-pulse">
+                <div class="h-4 bg-[var(--text-200)] rounded w-3/4 mb-4"></div>
+                <div class="h-4 bg-[var(--text-200)] rounded w-1/2 mb-4"></div>
+            </div>
+        </template>
+    </Suspense>
+
 </template>
 
 <script setup lang="ts">
@@ -21,7 +32,16 @@ interface BlogPost {
     _dir: string;
 }
 
-const postsState = useState<BlogPost[]>("posts", () => []);
+const { data: posts, status, error, refresh, clear } = await useLazyAsyncData("blog-posts", async () =>
+    await queryContent<BlogPost>("blog")
+        .where({
+            _partial: false,
+            _draft: false,
+            _path: { $ne: "/blog" }, // Exclude index page
+        })
+        .sort({ _path: 1 })
+        .find()
+);
 
 const shuffled = <T>(items: T[]): T[] =>
     items
