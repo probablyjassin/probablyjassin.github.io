ec6eb7e691eb48797fcbe8ced6031ef772e246a7
Jässin Aouani <aouanijassin@gmail.com>
2025-10-17 15:34:28 +0200

    increase spacing between nav items and their icons

--- app/app.vue
+++ app/app.vue
@@ -5,8 +5,9 @@
 				<template v-for="link in links" :key="link.name">
 					<li>
 						<NuxtLink :to="link.to" class="flex items-center group no-underline text-[var(--text-900)]">
-							<Icon :name="`line-md:${link.linemdicon}`" class="w-5 h-5 text-[var(--text-900)] group-hover:text-[var(--secondary-400)] mr-5" />
-							<span class="group-hover:text-[var(--secondary-400)]">{{ link.name }}</span>
+							<Icon :name="`line-md:${link.linemdicon}`"
+								class="w-8 h-5 text-[var(--text-900)] group-hover:text-[var(--secondary-400)]" />
+							<span class="group-hover:text-[var(--secondary-400)] mx-7">{{ link.name }}</span>
 						</NuxtLink>
 					</li>
 				</template>
@@ -24,10 +25,12 @@
 					<p class="text-[var(--text-900)]">© 2025 <span class="whitespace-nowrap">Jässin</span></p>
 				</div>
 				<div class="flex space-x-6">
-					<a title="github" href="https://github.com/probablyjassin" class="text-[var(--text-900)] hover:text-[var(--secondary-400)] transition-colors">
+					<a title="github" href="https://github.com/probablyjassin"
+						class="text-[var(--text-900)] hover:text-[var(--secondary-400)] transition-colors">
 						<Icon name="line-md:github" class="w-6 h-6" />
 					</a>
-					<a title="email" href="mailto:jassin@aouani.de" class="text-[var(--text-900)] hover:text-[var(--secondary-400)] transition-colors">
+					<a title="email" href="mailto:jassin@aouani.de"
+						class="text-[var(--text-900)] hover:text-[var(--secondary-400)] transition-colors">
 						<Icon name="line-md:email" class="w-6 h-6" />
 					</a>
 				</div>
@@ -37,98 +40,100 @@
 </template>
 
 <script setup lang="ts">
-	const links = [
-		{ name: "Home", to: "/", linemdicon: "home" },
-		{ name: "Projects", to: "/projects", linemdicon: "document-code" },
-		{ name: "Blog", to: "/blog", linemdicon: "text-box" },
-		{ name: "Github", to: "https://github.com/probablyjassin", linemdicon: "github-loop", href: true },
-	];
+const links = [
+	{ name: "Home", to: "/", linemdicon: "home" },
+	{ name: "Projects", to: "/projects", linemdicon: "document-code" },
+	{ name: "Blog", to: "/blog", linemdicon: "text-box" },
+	{ name: "Github", to: "https://github.com/probablyjassin", linemdicon: "github-loop", href: true },
+];
 </script>
 
 <style scoped lang="postcss">
-	.main-content {
-		padding-left: 1.5em;
+.main-content {
+	padding-left: 1.5em;
+}
+
+#navbar {
+	background-color: var(--primary-300);
+}
+
+ul {
+	list-style-type: none;
+}
+
+nav ul {
+	padding-top: 0.3rem;
+}
+
+nav ul li:first-child {
+	padding-right: 1rem;
+}
+
+nav ul li:not(:first-child) {
+	padding-left: 1rem;
+	padding-right: 1rem;
+}
+
+.placeholder {
+	height: 40px;
+	margin-bottom: 24px;
+	background-color: #f0f0f0;
+	animation: fadeInOut 0.5s infinite;
+}
+
+@keyframes fadeInOut {
+
+	0%,
+	100% {
+		opacity: 0.5;
 	}
 
-	#navbar {
-		background-color: var(--primary-300);
+	50% {
+		opacity: 1;
+	}
+}
+
+#navbar {
+	background-color: var(--primary-300);
+}
+
+ul {
+	list-style-type: none;
+}
+
+nav ul {
+	padding-top: 0.3rem;
+}
+
+#navbar ul li {
+	color: var(--text-900);
+}
+
+nav ul li:first-child {
+	padding-right: 1rem;
+}
+
+nav ul li:not(:first-child) {
+	padding-left: 1rem;
+	padding-right: 1rem;
+}
+
+.placeholder {
+	height: 40px;
+	margin-bottom: 24px;
+	background-color: #f0f0f0;
+	animation: fadeInOut 0.5s infinite;
+}
+
+@keyframes fadeInOut {
+
+	0%,
+	100% {
+		opacity: 0.5;
 	}
 
-	ul {
-		list-style-type: none;
+	50% {
+		opacity: 1;
 	}
-
-	nav ul {
-		padding-top: 0.3rem;
-	}
-
-	nav ul li:first-child {
-		padding-right: 1rem;
-	}
-
-	nav ul li:not(:first-child) {
-		padding-left: 1rem;
-		padding-right: 1rem;
-	}
-
-	.placeholder {
-		height: 40px;
-		margin-bottom: 24px;
-		background-color: #f0f0f0;
-		animation: fadeInOut 0.5s infinite;
-	}
-
-	@keyframes fadeInOut {
-		0%,
-		100% {
-			opacity: 0.5;
-		}
-
-		50% {
-			opacity: 1;
-		}
-	}
-
-	#navbar {
-		background-color: var(--primary-300);
-	}
-
-	ul {
-		list-style-type: none;
-	}
-
-	nav ul {
-		padding-top: 0.3rem;
-	}
-
-	#navbar ul li {
-		color: var(--text-900);
-	}
-
-	nav ul li:first-child {
-		padding-right: 1rem;
-	}
-
-	nav ul li:not(:first-child) {
-		padding-left: 1rem;
-		padding-right: 1rem;
-	}
-
-	.placeholder {
-		height: 40px;
-		margin-bottom: 24px;
-		background-color: #f0f0f0;
-		animation: fadeInOut 0.5s infinite;
-	}
-
-	@keyframes fadeInOut {
-		0%,
-		100% {
-			opacity: 0.5;
-		}
-
-		50% {
-			opacity: 1;
-		}
-	}
+}
 </style>
