076d482aa6ca1110c57931a502ac8b3c562464c9
probablyjassin <aouanijassin@gmail.com>
2025-10-18 19:49:08 +0200

    add nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
-	modules: ["@nuxt/content", "@nuxt/ui"],
+	modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image"],
 	css: ["~/assets/css/main.css", "~/assets/css/tailwind.css"],
 });
