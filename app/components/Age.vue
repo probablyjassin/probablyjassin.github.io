a8342dc3db019a3ecceb51704c874a03e9f7be59
probablyjassin <aouanijassin@gmail.com>
2025-10-19 21:10:35 +0200

    make my age display dynamic

---
+++ app/components/Age.vue
@@ -0,0 +1,11 @@
+<template>{{ age }}</template>
+
+<script setup>
+	const birthDate = new Date("2006-04-01");
+	const today = new Date();
+	let age = today.getFullYear() - birthDate.getFullYear();
+	const monthDiff = today.getMonth() - birthDate.getMonth();
+	if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+</script>
