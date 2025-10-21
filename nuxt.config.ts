497e6c233e1193ec13b0091b9aa42c95c8ad7087
Jässin Aouani <aouanijassin@gmail.com>
2025-10-21 13:15:14 +0200

    merge tailwind.css into main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
-	modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image"],
-	css: ["~/assets/css/main.css", "~/assets/css/tailwind.css"],
-	app: {
-		pageTransition: { name: "page", mode: "out-in" },
-		layoutTransition: { name: "layout", mode: "out-in" },
-	},
-	content: {
-		preview: {
-			api: "https://api.nuxt.studio",
-		},
-	},
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image"],
+  css: ["~/assets/css/main.css"],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+    },
+  },
 });
