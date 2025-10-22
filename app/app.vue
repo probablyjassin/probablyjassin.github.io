33c9be0578c57b163222b2d4d327507e23d8187e
probablyjassin <aouanijassin@gmail.com>
2025-10-23 00:17:50 +0200

    make transitions cooler

--- app/app.vue
+++ app/app.vue
@@ -30,22 +30,27 @@
 	/* Page Transitions */
 	.page-enter-active,
 	.page-leave-active {
-		transition: all 0.4s;
+		transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
 	}
 	.page-enter-from,
 	.page-leave-to {
 		opacity: 0;
-		filter: blur(1rem);
+		transform: translateY(-20px) scale(0.95);
 	}
 
 	/* Layout Transitions */
 	.layout-enter-active,
 	.layout-leave-active {
-		transition: all 0.5s ease;
+		transition: all 0.4s;
 	}
-	.layout-enter-from,
+
+	.layout-enter-from {
+		opacity: 0;
+		transform: translateY(20px) scale(0.95);
+	}
+
 	.layout-leave-to {
 		opacity: 0;
-		filter: blur(10px);
+		filter: blur(1rem);
 	}
 </style>
