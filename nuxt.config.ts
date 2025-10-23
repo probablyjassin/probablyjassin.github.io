bc5a12c288b2059d4a316e93b0707e4cce9c0d36
Jässin Aouani <aouanijassin@gmail.com>
2025-10-23 09:52:32 +0200

    enable view transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
+  experimental: {
+    viewTransition: true,
+  },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     layoutTransition: { name: "layout", mode: "out-in" },
