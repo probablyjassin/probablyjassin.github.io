d1ba358cfa6d26f4909e3a586b4c7b2808ddae2c
probablyjassin <aouanijassin@gmail.com>
2025-12-03 17:07:46 +0100

    install nuxt studio alpha

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image"],
-  css: ["~/assets/css/main.css"],
-  experimental: {
-    viewTransition: true,
-  },
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
-    layoutTransition: { name: "layout", mode: "out-in" },
-  },
-  content: {
-    preview: {
-      api: "https://api.nuxt.studio",
-    },
-    build: {
-      markdown: {
-        highlight: {
-          theme: "github-dark",
-          langs: [
-            "vue",
-            "typescript",
-            "javascript",
-            "bash",
-            "python",
-            "json",
-            "yml",
-            "diff",
-          ],
-        },
-      },
-    },
-  },
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/content", "nuxt-studio", "@nuxt/ui", "@nuxt/image"],
+	css: ["~/assets/css/main.css"],
+	experimental: {
+		viewTransition: true,
+	},
+	app: {
+		pageTransition: { name: "page", mode: "out-in" },
+		layoutTransition: { name: "layout", mode: "out-in" },
+	},
+	content: {
+		preview: {
+			api: "https://localhost:3000",
+		},
+		build: {
+			markdown: {
+				highlight: {
+					theme: "github-dark",
+					langs: ["vue", "typescript", "javascript", "bash", "python", "json", "yml", "diff"],
+				},
+			},
+		},
+	},
+	studio: {
+		repository: {
+			provider: "github",
+			owner: "probablyjassin",
+			repo: "probablyjassin.github.io",
+			branch: "main",
+			//rootDir: '' // optional: location of your content app
+		},
+	},
 });
