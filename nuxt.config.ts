91ff8a569a365753ae8b7133f5644abe0b3ad96e
Jässin <jaessin.aouani@wbs-it.de>
2026-01-06 10:31:36 +0100

    add nuxt studio 1.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
-	ssr: true,
-	modules: ["@nuxt/content", "nuxt-studio", "@nuxt/ui", "@nuxt/image"],
-	css: ["~/assets/css/main.css"],
-	experimental: {
-		viewTransition: true,
-	},
-	app: {
-		pageTransition: { name: "page", mode: "out-in" },
-		layoutTransition: { name: "layout", mode: "out-in" },
-	},
-	content: {
-		preview: {
-			api: "https://jaess.in",
-		},
-		build: {
-			markdown: {
-				highlight: {
-					theme: "github-dark",
-					langs: ["vue", "typescript", "javascript", "bash", "python", "json", "yml", "diff"],
-				},
-			},
-		},
-	},
-	studio: {
-		repository: {
-			provider: "github",
-			owner: "probablyjassin",
-			repo: "probablyjassin.github.io",
-			branch: "main",
-			//rootDir: '' // optional: location of your content app
-		},
-	},
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  ssr: true,
+  modules: ["@nuxt/content", "nuxt-studio", "@nuxt/ui", "@nuxt/image"],
+  css: ["~/assets/css/main.css"],
+  experimental: {
+    viewTransition: true,
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+  content: {
+    preview: {
+      api: "https://jaess.in",
+    },
+    build: {
+      markdown: {
+        highlight: {
+          theme: "github-dark",
+          langs: [
+            "vue",
+            "typescript",
+            "javascript",
+            "bash",
+            "python",
+            "json",
+            "yml",
+            "diff",
+          ],
+        },
+      },
+    },
+  },
+  studio: {
+    repository: {
+      provider: "github",
+      owner: "probablyjassin",
+      repo: "probablyjassin.github.io",
+      branch: "main",
+      //rootDir: '' // optional: location of your content app
+    },
+  },
 });
